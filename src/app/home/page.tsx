06685f224612826d98aa74689fcd4f097f00c310
waleedprogramer <waleedbinmehmoodahmed@gmail.com>
2024-10-26 13:50:11 +0500

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,7 +8,7 @@ function Main() {
   <div className="h-1/2 flex lg:flex-row justify-around flex-col pt-36">
     <div className="content flex flex-col justify-center text-center lg:w-1/2	">
       <h1 className='text-7xl font-bold text-indigo-600'>A creative design studio</h1>
-      <p className='mt-3 text-md'>We are a creative design studio specializing in meeting the needs of the new generation. We offer innovative and cutting-edge design solutions to help our clients stand out in today's fast-paced.</p>
+      <p className='mt-3 text-md'>We are a creative design studio specializing in meeting the needs of the new generation. We offer innovative and cutting-edge design solutions to help our clients stand out in todays fast-paced.</p>
       <span>
       <button className='bg-indigo-600 p-5 mt-5 rounded-full text-white text-lg font-bold hover:opacity-95'>Book a free consultation</button>
       </span>
